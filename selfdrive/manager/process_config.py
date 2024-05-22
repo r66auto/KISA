a8891f06ea0a4bf6f9ba50766cff3f3b02f1b13d
multikyd <72329880+multikyd@users.noreply.github.com>
2024-05-22 15:05:29 +0900

    merged master-ci

--- selfdrive/manager/process_config.py
+++ selfdrive/manager/process_config.py
@@ -48,7 +48,7 @@ EnableExternalNavi = Params().get("KISANaviSelect", encoding="utf8") == "1" or P
 NDM = Params().get_bool('DoNotDisturbMode')
 
 procs = [
-  DaemonProcess("manage_athenad", "selfdrive.athena.manage_athenad", "AthenadPid"),
+  DaemonProcess("manage_athenad", "system.athena.manage_athenad", "AthenadPid"),
 
   NativeProcess("camerad", "system/camerad", ["./camerad"], driverview),
   #NativeProcess("logcatd", "system/logcatd", ["./logcatd"], only_onroad),
@@ -70,6 +70,7 @@ procs = [
   PythonProcess("calibrationd", "selfdrive.locationd.calibrationd", only_onroad),
   PythonProcess("torqued", "selfdrive.locationd.torqued", only_onroad),
   PythonProcess("controlsd", "selfdrive.controls.controlsd", only_onroad),
+  PythonProcess("card", "selfdrive.car.card", only_onroad),
   #PythonProcess("deleter", "system.loggerd.deleter", always_run),
   #PythonProcess("dmonitoringd", "selfdrive.monitoring.dmonitoringd", driverview, enabled=(not PC or WEBCAM)),
   PythonProcess("qcomgpsd", "system.qcomgpsd.qcomgpsd", qcomgps, enabled=TICI),
@@ -81,13 +82,13 @@ procs = [
   PythonProcess("pigeond", "system.ubloxd.pigeond", ublox, enabled=TICI),
   PythonProcess("plannerd", "selfdrive.controls.plannerd", only_onroad),
   PythonProcess("radard", "selfdrive.controls.radard", only_onroad),
-  PythonProcess("thermald", "selfdrive.thermald.thermald", always_run),
-  #PythonProcess("tombstoned", "selfdrive.tombstoned", always_run, enabled=not PC),
+  PythonProcess("thermald", "system.thermald.thermald", always_run),
+  #PythonProcess("tombstoned", "system.tombstoned", always_run, enabled=not PC),
   #PythonProcess("updated", "selfdrive.updated.updated", only_offroad, enabled=not PC),
   #PythonProcess("uploader", "system.loggerd.uploader", always_run),
-  #PythonProcess("statsd", "selfdrive.statsd", always_run),
+  #PythonProcess("statsd", "system.statsd", always_run),
 
-  PythonProcess("kupdate", "selfdrive.kupdate", always_run),
+  PythonProcess("kupdate", "system.kupdate", always_run),
 
   # debug procs
   NativeProcess("bridge", "cereal/messaging", ["./bridge"], notcar),
